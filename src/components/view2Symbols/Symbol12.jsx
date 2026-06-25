ca2d4967c08bc652407a4a74b8363ed3bda5edc5
floyd910 <lgelava7@gmail.com>
2026-06-25 13:38:05 +0400

    symbols 9 and 12 changed

--- src/components/view2Symbols/Symbol12.jsx
+++ src/components/view2Symbols/Symbol12.jsx
@@ -1,11 +1,15 @@
-import { View2SymbolBase, view2SymbolAsset, view2SymbolFrames } from "./View2SymbolBase.jsx";
+import {
+  View2SymbolBase,
+  view2SymbolAsset,
+  view2SymbolFrames,
+} from "./View2SymbolBase.jsx";
 
 export const symbol12Assets = {
-  staticImage: view2SymbolAsset(12, "1.png"),
+  staticImage: view2SymbolAsset(12, "4.png"),
   background: view2SymbolAsset(12, "background.png"),
-  winFrames: view2SymbolFrames(12, 8),
+  winFrames: view2SymbolFrames(12, 12),
   forwardLoop: true,
-  frameMs: 250,
+  frameMs: 167,
 };
 
 export default function Symbol12(props) {
