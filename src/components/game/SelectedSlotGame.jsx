a8f2446ccb58fad659a5342ee662a7064f194a92
floyd910 <lgelava7@gmail.com>
2026-06-30 21:46:48 +0400

    removed go back button

--- src/components/game/SelectedSlotGame.jsx
+++ src/components/game/SelectedSlotGame.jsx
@@ -5,12 +5,5 @@ import "../../App.css";
 export default function SelectedSlotGame({ slotId, onBack }) {
   const controller = useGameController(slotId);
 
-  return (
-    <>
-      <button type="button" className="child-slot-back" onClick={onBack}>
-        Slots
-      </button>
-      <GameShell controller={controller} onBackToSlots={onBack} />
-    </>
-  );
+  return <GameShell controller={controller} onBackToSlots={onBack} />;
 }
