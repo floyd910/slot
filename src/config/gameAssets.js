6363c0ce0e63d9be05fbcc40721ca017a5174df2
floyd910 <lgelava7@gmail.com>
2026-08-17 19:17:38 +0400

    loaders

--- src/config/gameAssets.js
+++ src/config/gameAssets.js
@@ -28,12 +28,12 @@ export const GAME1_VIEW2_SYMBOL_8_STATIC_SRC =
   "/assets/img/game1-korvonsaroi-karavan-view2-symbol8-static.webp";
 export const GAME1_VIEW2_SYMBOL_11_STATIC_SRC =
   "/assets/img/game1-korvonsaroi-karavan-view2-symbol11-static.webp";
-export const GAME4_COVER_SRC = "/assets/img/chooser/game4-background.avif";
+export const GAME4_COVER_SRC = "/assets/img/game4-background-lossless.webp";
 export const GAME4_LOGO_SRC = "/assets/img/game4-header-optimized.webp";
 export const GAME4_CHOOSER_TILE_SRC = "/assets/img/chooser/game4.webp";
 export const GAME4_VIEW1_CELL_BACKGROUND_SRC = "/assets/img/game4-view1-cell-background-optimized.webp";
 export const GAME4_VIEW1_HIGHLIGHT_BACKGROUND_SRC = "/assets/img/game4-view1-highlight-background-optimized.webp";
-export const GAME4_VIEW2_INFO_BACKGROUND_SRC = "/assets/img/game4-view2-info-background-optimized.webp";
+export const GAME4_VIEW2_INFO_BACKGROUND_SRC = "/assets/img/game4-view2-symbol1-background-optimized.webp";
 export const GAME4_VIEW2_INFO_SMALL_BOX_BACKGROUND_SRC = "/assets/img/game4-view2-info-small-box-background-optimized.webp";
 export const GAME4_DOUBLE_SCENE_BACKGROUND_SRC = "/assets/img/game4-double-scene-bg.webp";
 export const GAME4_DOUBLE_SCENE_CLOSED_CHEST_SRC = "/assets/img/game4-double-closed-chest.webp";
@@ -54,7 +54,7 @@ export const GAME4_VIEW2_CELL_BACKGROUND_ASSETS = [
   "/assets/img/game4-view2-symbol5-background-optimized.webp",
   "/assets/img/game4-view2-symbol6-background-optimized.webp",
 ];
-export const GAME6_COVER_SRC = "/assets/img/chooser/game6-background.avif";
+export const GAME6_COVER_SRC = "/assets/img/game6-background-lossless.webp";
 export const GAME6_LOGO_SRC = "/assets/img/game6-header-optimized.webp";
 export const GAME6_CHOOSER_TILE_SRC = "/assets/img/chooser/game6.webp";
 export const GAME7_CHOOSER_TILE_SRC = "/assets/img/chooser/game7-babylon.webp";
@@ -82,7 +82,7 @@ export const GAME7_VIEW2_SYMBOL_7_STATIC_SRC =
 export const GAME7_VIEW2_SYMBOL_8_STATIC_SRC =
   "/assets/img/game7-babylon-view2-symbol8-static.webp";
 export const GAME5_CHOOSER_TILE_SRC = "/assets/img/chooser/game5.webp";
-export const GAME5_COVER_SRC = "/assets/img/chooser/game5-background.avif";
+export const GAME5_COVER_SRC = "/assets/img/game5-background-lossless.webp";
 export const GAME5_LOGO_SRC = "/assets/img/game5-header-optimized.webp";
 export const GAME5_VIEW1_CELL_BACKGROUND_SRC = "/assets/img/game5-view1-cell-background-optimized.webp";
 export const GAME5_VIEW1_HIGHLIGHT_BACKGROUND_SRC = "/assets/img/game5-view1-highlight-background-optimized.webp";
@@ -120,7 +120,7 @@ export const GAME6_VIEW2_CELL_BACKGROUND_ASSETS = [
 
 
 export const GAME2_CHOOSER_TILE_SRC = "/assets/img/chooser/game2.webp";
-export const GAME2_COVER_SRC = "/assets/img/chooser/game2-background.avif";
+export const GAME2_COVER_SRC = "/assets/img/game2-background-lossless.webp";
 export const GAME2_LOGO_SRC = "/assets/img/game2-header-logo.webp";
 export const GAME2_VIEW2_SYMBOL_1_BACKGROUND_SRC = "/assets/img/game2-view2-symbol1-background.webp";
 export const GAME2_VIEW2_SYMBOL_4_BACKGROUND_SRC = "/assets/img/game2-view2-symbol4-background.webp";
@@ -148,7 +148,7 @@ export const SLOT_CHOOSER_TILE_ASSETS = [
   GAME6_CHOOSER_TILE_SRC,
   GAME7_CHOOSER_TILE_SRC,
 ];
-export const GAME3_COVER_SRC = "/assets/img/chooser/game3-background.avif";
+export const GAME3_COVER_SRC = "/assets/img/game3-background-lossless.webp";
 export const GAME3_LOGO_SRC = "/assets/img/game3-logo-optimized.webp";
 export const GAME3_LOGO_ORNAMENT_SRC = "/assets/img/game3-logo-ornament.webp";
 export const GAME3_SHELL_ASSETS = [GAME3_COVER_SRC, GAME3_LOGO_SRC];
@@ -173,36 +173,6 @@ export const LANGUAGE_CHOOSER_ASSETS = [
   "/img/header/language-arrow.png",
 ];
 
-const GAME_ASSET_DIR = "/img/extracted/\u0438\u0433\u0440\u0430-\u0425\u0443\u0448\u043a\u043e\u043b-\u044d\u043b\u0435\u043c\u0435\u043d\u0442\u044b-\u0438\u0433\u0440\u044b-1_0";
-const PAYTABLE_ASSET_DIR = "/img/extracted/\u0438\u0433\u0440\u0430-\u0425\u0443\u0448\u043a\u043e\u043b-\u044d\u043b\u0435\u043c\u0435\u043d\u0442\u044b-\u0442\u0430\u0431\u043b\u0438\u0446\u0430-\u0432\u044b\u0438\u0433\u0440\u044b\u0448\u0435\u0439-1_1";
-const SIDE_BUTTON_ASSET_DIR = "/img/extracted/\u0442\u0443\u0442-\u043a\u043d\u043e\u043f\u043a\u0438-\u0441\u043f\u0440\u0430\u0432\u0430-1_0";
-const DOUBLE_SCENE_ASSET_DIR = "/img/extracted/\u0442\u0443\u0442-\u0444\u043e\u043d--\u0432\u044b\u0431\u043e\u0440-\u0438\u0433\u0440-\u043f\u0435\u0440\u0432\u0430\u044f-\u0441\u0442\u0440\u0430\u043d\u0438\u0446\u0430-\u0438-\u0441\u0440\u0430\u0437\u0443-\u043b\u043e\u0442\u043e\u0440\u0435\u0439\u043d\u044b\u0439-\u0440\u0435\u0436\u0438\u043c_\u0443\u0434\u0432\u043e\u0435\u043d\u0438\u0435-5_0";
-const VIEW1_GRID_ASSET_DIR = DOUBLE_SCENE_ASSET_DIR.replace(
-  "_\u0443\u0434\u0432\u043e\u0435\u043d\u0438\u0435-5_0",
-  "_\u0443\u0434\u0432\u043e\u0435\u043d\u0438\u0435-7_0",
-);
-
-export const GAME_AREA_BACKGROUND_SRC =
-  "/assets/img/game-area-background-optimized.webp";
-export const GAME_AREA_FOOTER_SRC =
-  GAME_ASSET_DIR + "/sprite_015_1282x196_at_1_1029.webp";
-export const GAME_HEADER_SRC =
-  GAME_ASSET_DIR + "/sprite_003_398x172_at_1492_1.png";
-
-export const VIEW1_GRID_ASSETS = [
-  "sprite_004_101x101_at_302_606.png",
-  "sprite_008_67x67_at_352_835.png",
-  "sprite_010_101x101_at_249_875.png",
-  "sprite_012_101x101_at_249_978.png",
-].map((file) => VIEW1_GRID_ASSET_DIR + "/" + file);
-
-export const BOTTOM_BAR_ASSETS = Array.from({ length: 12 }, (_, index) =>
-  SIDE_BUTTON_ASSET_DIR +
-  `/sprite_${String(index + 1).padStart(3, "0")}_${index < 3 || (index >= 6 && index <= 8) ? "284x152" : "284x102"}_at_1_${[
-    1, 155, 309, 463, 567, 671, 775, 929, 1083, 1237, 1341, 1445,
-  ][index]}.png`,
-);
-
 export const DOUBLE_SCENE_ASSET_SOURCES = Object.freeze({
   background: "/assets/img/double-scene-bg.png",
   emptyChest: "/assets/img/double-empty-chest.png",
@@ -214,16 +184,10 @@ export const DOUBLE_SCENE_ASSET_SOURCES = Object.freeze({
 export const DOUBLE_SCENE_ASSETS = Object.values(DOUBLE_SCENE_ASSET_SOURCES);
 
 export const SHARED_FIRST_PAINT_ASSETS = [
-  // Every visible element of the shared main game screen.
-  GAME_AREA_BACKGROUND_SRC,
-  GAME_AREA_FOOTER_SRC,
-  GAME_HEADER_SRC,
   "/img/ui/game-menu-close.png",
   LOTTERY_PAINTBRUSH_SRC,
   GAME_HOME_ICON_SRC,
   ...LANGUAGE_CHOOSER_ASSETS,
-  ...VIEW1_GRID_ASSETS,
-  ...BOTTOM_BAR_ASSETS,
 ];
 
 const GAME2_VIEW1_ASSETS = [
@@ -276,7 +240,6 @@ export const FIRST_PAINT_GAME_IMAGE_ASSETS = SHARED_FIRST_PAINT_ASSETS;
 export const DEFERRED_GAME_IMAGE_ASSETS = [
   GAME3_LOGO_ORNAMENT_SRC,
   ...DOUBLE_SCENE_ASSETS,
-  PAYTABLE_ASSET_DIR + "/sprite_001_1282x1026_at_1_1.webp",
   "/img/eldorado-winnings-table-bg.webp",
   "/img/eldorado-winnings-title-bg.webp",
 ];
